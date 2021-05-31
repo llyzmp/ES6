a0dda4e897dfe782a83289cfd41c3e54ca14ef39
zmplly <1776321381@qq.com>
2021-05-31 23:04:52 +0800

    promise的后续处理

--- 9.异步处理/7.手写Promise/MyPromise.js
+++ 9.异步处理/7.手写Promise/MyPromise.js
@@ -1,18 +1,21 @@
 const MyPromise = (()=>{
   const PENDING = 'pending',
-  RESOLVED = 'resolve',
+  RESOLVED = 'resolved',
   REJECTED = 'rejected',
   PromiseValue = Symbol('PromiseValue'),  // 状态数据
   PromiseStatus = Symbol('PromiseStatus'), // 当前状态
-  changeStatus = Symbol('changeStatus'); //当前状态
-
+  changeStatus = Symbol('changeStatus'), //当前状态
+  thenables = Symbol('thenables'),  // thenable
+  catchables = Symbol('catchables'), // catchable
+  settleHandle = Symbol('settleHandle'); // 后续处理提取公共函数
   return class MyPromise{
     /**
      * 改变当前Promise状态
      * @param {*} newStatus 
      * @param {*} newValue 
+     * @param {*} queue   // 执行的作业队列,写完后续函数后添加的参数
      */
-    [changeStatus](newStatus,newValue) {
+    [changeStatus](newStatus,newValue, queue) {
       // 状态必须从pending
       if(this[PromiseStatus] !== PENDING) {
         // 状态无法变更
@@ -22,7 +25,11 @@ const MyPromise = (()=>{
        this[PromiseStatus] = newStatus;
        // 把值传进去
        this[PromiseValue] = newValue;
+
+       // 当状态从pending变更时,需要执行相应队列中的函数, handle是队列中的函数
+      queue.forEach(handle => handle(newValue));
     }
+
     /**
      * 
      * @param {*} executor 未决阶段(pending状态)下的处理函数
@@ -30,13 +37,18 @@ const MyPromise = (()=>{
     constructor(executor) {
       this[PromiseStatus] = PENDING;
       this[PromiseValue] = undefined;
+      this[thenables] = []; // 后续处理函数的数组 -> resolved
+      this[catchables] = []; // 后续处理函数的数组 -> rejected
+
       // resolve函数
       const resolve = (data) => {
-        this[changeStatus](RESOLVED,data);
+        // 状态值,数据,队列
+        this[changeStatus](RESOLVED,data, this[thenables]);
       }
       // reject函数
       const reject = (reason) => {
-        this[changeStatus](RESOLVED,reason);
+        // 状态值,数据,队列
+        this[changeStatus](REJECTED,reason,this[catchables]);
       }
 
 
@@ -51,6 +63,39 @@ const MyPromise = (()=>{
         reject(err);
       }
     }
+    // 内部方法
+    /**
+     * 处理,后续处理公共函数
+     * @param {*} handle 后续处理函数
+     * @param {*} immediatelyStatus 需要立即执行的状态
+     * @param {*} queue 作业队列
+     */
+    [settleHandle](handle,immediatelyStatus,queue) {
+      // 如果handle不是函数时,return
+      if(typeof handle !== 'function') {
+        return;
+      }
+       // 运行时判断是否为resolved或者rejected,是的话直接运行传入的函数handle
+       if(this[PromiseStatus] === immediatelyStatus) {
+        // 把值传进去,值为this[PromiseValue]
+        // 异步处理应该放到微队列,浏览器环境不能,模拟一个异步队列(宏队列),加个定时器
+        setTimeout(()=>{
+          handle(this[PromiseValue]);
+        },0)
+      }else {
+        queue.push(handle)
+      }
+    }
+    // then方法
+    then(thenable,catchable) {
+     this[settleHandle](thenable,RESOLVED,this[thenables]);
+     this.catch(catchable);
+    }
+    // catch方法
+    catch(catchable) {
+      this[settleHandle](catchable,REJECTED,this[catchables]);
+    }
+
   
   }
 })();
